f3f0c088de48afebeed363b0c7a0093093adbe1b
aaron <aaronelectricaltiger@gmail.com>
2025-07-05 11:23:29 -0400

    game screen ui

--- script.js
+++ script.js
@@ -4,30 +4,41 @@ document.querySelector('.play').addEventListener('click', function() {
     main.classList.remove('start');
     main.classList.add('game');
     main.innerHTML = `
-        <h1>Blackjack</h1>
         <div class="game">
-        <div class="dealer-cards-container">
-            <div id="dealer-card0" class="dealer-card"><img src="cardback.png"></div>
-            <div id="dealer-card1" class="dealer-card"><img src="cardback.png"></div>
-            <div id="dealer-card2" class="dealer-card"><img src="cardback.png"></div>
-            <div id="dealer-card3" class="dealer-card"><img src="cardback.png"></div>
-            <div id="dealer-card4" class="dealer-card"><img src="cardback.png"></div>
-            <div id="dealer-card5" class="dealer-card"><img src="cardback.png"></div>
-            <div id="dealer-card6" class="dealer-card"><img src="cardback.png"></div>
-            <div id="dealer-card7" class="dealer-card"><img src="cardback.png"></div>
-        </div>
+            <div class="dealer-cards-container">
+                <div id="dealer-card0" class="dealer-card"><img src="cardback.png"></div>
+                <div id="dealer-card1" class="dealer-card"><img src="cardback.png"></div>
+                <div id="dealer-card2" class="dealer-card"><img src="cardback.png"></div>
+                <div id="dealer-card3" class="dealer-card"><img src="cardback.png"></div>
+                <div id="dealer-card4" class="dealer-card"><img src="cardback.png"></div>
+                <div id="dealer-card5" class="dealer-card"><img src="cardback.png"></div>
+                <div id="dealer-card6" class="dealer-card"><img src="cardback.png"></div>
+                <div id="dealer-card7" class="dealer-card"><img src="cardback.png"></div>
+            </div>
 
-        <div class="dealer-total">Total: <span id="dealer-total">??</span></div>
+            <div class="center-container">
+                <div class="dealer-total">Dealer Total: <span id="dealer-total">??</span></div>
+                <div class="wins-counter">
+                    <div>Your Wins: <span id="player-wins">0</span></div>
+                    <div>Dealer Wins: <span id="dealer-wins">0</span></div>
+                </div>
+                <div class="player-side">
+                    <button class="hit-button">Hit</button>
+                    <div class="player-total">Player Total: <span id="player-total">??</span></div>
+                    <button class="stand-button">Stand</button>
+                </div>
+            </div>
 
-        <div class="player-cards-container">
-            <div id="player-card0" class="player-card"><img src="cardback.png"></div>
-            <div id="player-card1" class="player-card"><img src="cardback.png"></div>
-            <div id="player-card2" class="player-card"><img src="cardback.png"></div>
-            <div id="player-card3" class="player-card"><img src="cardback.png"></div>
-            <div id="player-card4" class="player-card"><img src="cardback.png"></div>
-            <div id="player-card5" class="player-card"><img src="cardback.png"></div>
-            <div id="player-card6" class="player-card"><img src="cardback.png"></div>
-            <div id="player-card7" class="player-card"><img src="cardback.png"></div> 
+            <div class="player-cards-container">
+                <div id="player-card0" class="player-card"><img src="cardback.png"></div>
+                <div id="player-card1" class="player-card"><img src="cardback.png"></div>
+                <div id="player-card2" class="player-card"><img src="cardback.png"></div>
+                <div id="player-card3" class="player-card"><img src="cardback.png"></div>
+                <div id="player-card4" class="player-card"><img src="cardback.png"></div>
+                <div id="player-card5" class="player-card"><img src="cardback.png"></div>
+                <div id="player-card6" class="player-card"><img src="cardback.png"></div>
+                <div id="player-card7" class="player-card"><img src="cardback.png"></div> 
+            </div>
         </div>
     `;
 });
